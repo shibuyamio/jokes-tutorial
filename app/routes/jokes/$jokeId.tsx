0bb77fc74abdc64f30443cdb7a2fe49038fddf4a
shibuyamio <mio.shibuya@trictrac.co.jp>
2022-05-02 18:34:29 +0900

    mutations

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,30 @@
+import type { Joke } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Pick<Joke, "content"> | null };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  console.log(params);
+
+  const data: LoaderData = {
+    joke: await db.joke.findUnique({
+      select: { content: true },
+      where: { id: params.jokeId },
+    }),
+  };
+
+  return data;
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke?.content}</p>
     </div>
   );
 }
